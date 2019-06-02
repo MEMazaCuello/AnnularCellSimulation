ac3c0bc8aada13a79f23c95f2bbad33652946dad
MEMazaCuello <50495763+MEMazaCuello@users.noreply.github.com>
2019-06-02 17:58:23 +0200

    Update documentation

--- montecarloRoutines.h
+++ montecarloRoutines.h
@@ -1,10 +1,46 @@
+/****
+  'montecarloRoutines.h':
+  --------------------
+  Implementation of 'montecarloRoutines'.
+  For declaration details, see 'montecarloRoutines.cpp'.
+  
+  Needs: 'AnnularCell.h'
+  
+  --------------------
+  The 'montecarloRoutines' define how a Monte Carlo step
+  is done in the 'AnnularCell' representing the system.
+  It also include functions that compute and save the 
+  local order parameters -- nematic, tetratic and smectic, 
+  of the liquid crystal of 'Rod's inside the 'AnnularCell'. 
+        
+  --------------------
+  Last modified: 2019-06-02 
+  By: M. E. Maza Cuello
+****/
+
 #ifndef MONTECARLOROUTINES_H_INCLUDED
 #define MONTECARLOROUTINES_H_INCLUDED
 
 #include "AnnularCell.h"
 
+/* Methods */
+
 void stepMontecarlo(AnnularCell& cell);
-void getOrderParameters(AnnularCell& cell);
+  /**
+    'stepMontecarlo': Perform a Monte Carlo step on the 'cell'.
+                      I. e. for each 'Rod' inside the 'cell', try
+                      to randomly make a (small) change in its 
+                      position and orientation.
+                      The changes' amplitude is dynamically adjusted
+                      so that the acceptation probability is ~ 50 %.
+  **/
+
 void getOrderParameters(AnnularCell& cell, std::string& filename);
+  /**
+    'getOrderParameters': Compute the local nematic, tetratic and smectic 
+                          order parameters of the liquid crystal of 'Rod's. 
+                          Save the configuration and the order parameters
+                          in a .txt file called 'filename.txt'.
+  **/
 
 #endif // MONTECARLOROUTINES_H_INCLUDED
