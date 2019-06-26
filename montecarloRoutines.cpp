c23465f91bcc701c039274c2ea9c782fb94c473c
MEMazaCuello <50495763+MEMazaCuello@users.noreply.github.com>
2019-06-26 22:53:42 +0200

    'stepMontecarloNeighbourhood' included
    Now the MC step can be done faster by working by neighbourhoods instead of rod-by-rod.

--- montecarloRoutines.cpp
+++ montecarloRoutines.cpp
@@ -140,6 +140,115 @@ void stepMontecarlo(AnnularCell& cell)
   DELTA_ANGLE *= (0.5d+double(numSuccess)*INV_NUMBER_OF_RODS);
 }
 
+void stepMontecarloNeighbourhood(AnnularCell& cell)
+{
+    /**
+        'stepMontecarloNeighbourhood': Perform a Monte Carlo step on the 'cell'.
+                                        I. e. for each grid neighborhood inside the 'cell', try
+                                        to randomly make a (small) change in its
+                                        'Rod's' position and orientation.
+                                        The changes' amplitudes are dynamically adjusted
+                                        so that the acceptation probability is ~ 50 %.
+    **/
+
+    // Auxiliary constant variables
+    static const double MAX_RADIUS = OUTER_RADIUS-HALF_DIAGONAL;
+    static const double INV_NUMBER_OF_RODS = 1.0d/NUMBER_OF_RODS;
+    // Auxiliary variables:
+    static double DELTA_SPACE = 0.1*LENGTH;   // Maximum variation of coordinates
+    static double DELTA_ANGLE = 0.1*HALF_PI;  // Maximum variation of orientation
+
+    // Auxiliary array of indexes of the 'Rod's
+    std::vector<int> indexes;
+    // Boolean used to exit loops and mark new position as valid
+    bool validPosition;
+    // Number of 'Rod's that have been successfully moved
+    int  numSuccess = 0;
+
+    static std::vector<int> gridBoxes(cell.m_grid.m_BOXES*cell.m_grid.m_BOXES);
+
+    // Generate random index permutation
+    std::iota(gridBoxes.begin(), gridBoxes.end(), 0);
+    std::shuffle(gridBoxes.begin(), gridBoxes.end(), g);
+
+   int gi, gj;
+   for(int gbox : gridBoxes)
+   {
+    gi = gbox % cell.m_grid.m_BOXES;
+    gj = gbox / cell.m_grid.m_BOXES;
+
+    indexes = cell.m_grid.getBox(gi, gj);
+    cell.m_grid.m_neighbours = cell.m_grid.getNeighbours(gi, gj);
+    for(int i : indexes)
+    {
+      // Get 'Rod' with index 'i'
+      cell.m_aux_rod = cell.getRod(i);
+
+      /* Add random displacement */
+      // Coordinates (max displacement = DELTA_SPACE)
+      cell.m_aux_rod.m_xPos  += dist(g)*DELTA_SPACE;
+      cell.m_aux_rod.m_yPos  += dist(g)*DELTA_SPACE;
+      // Orientation (max displacement = DELTA_ANGLE)
+      cell.m_aux_rod.m_angle += dist(g)*DELTA_ANGLE;
+
+      /* Adjust positions to fit inside the 'AnnularCell' */
+      // Coordinates in interval [-OUTER_RADIUS, OUTER_RADIUS]
+      if(cell.m_aux_rod.m_xPos > OUTER_RADIUS){ cell.m_aux_rod.m_xPos = MAX_RADIUS;}
+      else if(cell.m_aux_rod.m_xPos < - OUTER_RADIUS){ cell.m_aux_rod.m_xPos = -MAX_RADIUS;}
+      if(cell.m_aux_rod.m_yPos > OUTER_RADIUS){ cell.m_aux_rod.m_yPos = MAX_RADIUS;}
+      else if(cell.m_aux_rod.m_yPos < - OUTER_RADIUS){ cell.m_aux_rod.m_yPos = -MAX_RADIUS;}
+      // Orientation in [-HALF_PI, HALF_PI]
+      while(cell.m_aux_rod.m_angle > HALF_PI)
+      {
+        cell.m_aux_rod.m_angle -= PI;
+      }
+      while(cell.m_aux_rod.m_angle < -HALF_PI)
+      {
+        cell.m_aux_rod.m_angle += PI;
+      }
+
+      // Assume the new position is valid
+      validPosition = true;
+      // If 'm_aux_rod' is touching inner or outer walls of 'cell',
+      if(cell.rodIsTouchingInnerWall(cell.m_aux_rod)||cell.rodIsTouchingOuterWall(cell.m_aux_rod))
+      {
+        // ... new position is not valid
+        validPosition = false;
+      }else{
+        // If not, for each neighbour
+        for(int idx : cell.m_grid.m_neighbours)
+        {
+          if(idx != i)
+          {
+            // ... check if is touching any neighbour...
+            if(cell.m_aux_rod.isTouchingRod(cell.getRod(idx)))
+            {
+              // ... and if it is, new position is not valid
+              validPosition = false;
+              break;
+            }
+          }
+        }
+      }
+
+      // If position is valid
+      if(validPosition)
+      {
+        // ... move index to new coordinates in the 'm_grid'
+        cell.m_grid.moveIndex(i,cell.m_grid.getGridCoords(cell.m_bundle[i]), cell.m_grid.getGridCoords(cell.m_aux_rod));
+        // ... save 'm_aux_rod' in the corresponding index
+        cell.m_bundle[i]= cell.m_aux_rod;
+        // ... and add one success
+        numSuccess++;
+      }
+    }
+  }
+
+  /* Correct maximum displacements to generate ~ 50 % acceptance */
+  DELTA_SPACE *= (0.5d+double(numSuccess)*INV_NUMBER_OF_RODS);
+  DELTA_ANGLE *= (0.5d+double(numSuccess)*INV_NUMBER_OF_RODS);
+}
+
 void getOrderParameters(AnnularCell& cell, std::string& filename){
   /**
     'getOrderParameters': Compute the local nematic, tetratic and smectic 
