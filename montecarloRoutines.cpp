9aab09035436708d8f029a75318f08a333dc7e7f
MEMazaCuello <50495763+MEMazaCuello@users.noreply.github.com>
2021-05-09 20:28:37 +0200

    Update montecarloRoutines.cpp

--- montecarloRoutines.cpp
+++ montecarloRoutines.cpp
@@ -7,7 +7,7 @@
   * Needs: "montecarloRoutines.h".
   *
   * --------------------
-  * Last modified: 2020-04-30
+  * Last modified: 2021-05-09
   * By: M. E. Maza-Cuello
   */
 
@@ -16,6 +16,7 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <iostream>
 
 #include "montecarloRoutines.h"
 
@@ -40,55 +41,25 @@ void stepMontecarlo(AnnularCell& cell)
   static double DELTA_SPACE = 0.1*LENGTH;   // Maximum variation of coordinates
   static double DELTA_ANGLE = 0.1*HALF_PI;  // Maximum variation of orientation
   static std::vector<int> indexes(NUMBER_OF_RODS);
+  static int  numSuccess;
 
-  bool validPosition;
-  int  numSuccess = 0;
-  std::vector<int> oldCoords;
-  std::vector<int> newCoords;
+  numSuccess = 0;
 
   // Generate random index permutation
   std::iota(indexes.begin(), indexes.end(), 0);
   std::shuffle(indexes.begin(), indexes.end(), g);
 
-  for(int i : indexes)
+  for (int& i : indexes)
   {
-    cell.m_aux_rod = cell.getRod(i);
-
-    // Old grid coordinates (before moving)
-    oldCoords = cell.m_grid.getCoords(cell.m_aux_rod);
-
+    // Move rod
+    cell.m_aux_rod = cell.m_bundle.at(i);
     cell.m_aux_rod.translate(dist(g)*DELTA_SPACE,dist(g)*DELTA_SPACE);
     cell.m_aux_rod.rotate(dist(g)*DELTA_ANGLE); // Also ensures that rod's angle is in [-pi/2,pi/2]
 
-    if ( cell.rodIsOutsideWalls(cell.m_aux_rod) )
+    // Check validity and save position
+    if (cell.isInternalPositionValid(i))
     {
-      continue;
-    }
-
-    // New grid coordinates (after moving)
-    newCoords = cell.m_grid.getCoords(cell.m_aux_rod);
-
-    cell.m_grid.m_neighbors = cell.m_grid.getNeighbors(newCoords);
-
-    validPosition = true;
-    for (int idx : cell.m_grid.m_neighbors)
-    {
-      if (idx != i)  // Rod is neighbor of itself
-      {
-        if (cell.m_aux_rod.isTouchingRod(cell.getRod(idx)))
-        {
-          validPosition = false;
-          break;
-        }
-      }
-    }
-
-    if (validPosition)
-    {
-      // Update grid coordinates of rod
-      cell.m_grid.moveIndex(i, oldCoords, newCoords);
-      cell.m_bundle[i]= cell.m_aux_rod;
-      numSuccess++;
+      ++numSuccess;
     }
   }
 
@@ -103,63 +74,27 @@ void stepMontecarloNeighborhood(AnnularCell& cell)
   static double DELTA_SPACE = 0.1*LENGTH;   // Maximum variation of coordinates
   static double DELTA_ANGLE = 0.1*HALF_PI;  // Maximum variation of orientation
   static std::vector<int> gridBoxes(cell.m_grid.m_BOXES*cell.m_grid.m_BOXES);
+  static int  numSuccess;
 
-  std::vector<int> indexes;
-  bool validPosition;
-  int  numSuccess = 0;
-  std::vector<int> oldCoords;
-  std::vector<int> newCoords;
+  numSuccess = 0;
 
   // Random boxes permutation
   std::iota(gridBoxes.begin(), gridBoxes.end(), 0);
   std::shuffle(gridBoxes.begin(), gridBoxes.end(), g);
 
-  int gi, gj;
-  for (int gbox : gridBoxes)
+  for (int& gbox : gridBoxes)
   {
-    gi = gbox % cell.m_grid.m_BOXES;
-    gj = gbox / cell.m_grid.m_BOXES;
-
-    cell.m_grid.m_neighbors = cell.m_grid.getNeighbors(gi, gj);
-
-    indexes = cell.m_grid.getBox(gi, gj);
-    for (int i : indexes)
+    for (int& i : cell.m_grid.getBox(gbox % cell.m_grid.m_BOXES, gbox / cell.m_grid.m_BOXES))
     {
-      cell.m_aux_rod = cell.getRod(i);
-
-      // Old grid coordinates (before moving)
-      oldCoords = cell.m_grid.getCoords(cell.m_aux_rod);
-
+      // Move rod
+      cell.m_aux_rod = cell.m_bundle.at(i);
       cell.m_aux_rod.translate(dist(g)*DELTA_SPACE,dist(g)*DELTA_SPACE);
       cell.m_aux_rod.rotate(dist(g)*DELTA_ANGLE); // Also ensures that rod's angle is in [-pi/2,pi/2]
 
-      if ( cell.rodIsOutsideWalls(cell.m_aux_rod) )
+      // Check validity and save position
+      if (cell.isInternalPositionValid(i))
       {
-        continue;
-      }
-
-      // New grid coordinates (after moving)
-      newCoords = cell.m_grid.getCoords(cell.m_aux_rod);
-
-      validPosition = true;
-      for (int idx : cell.m_grid.m_neighbors)
-      {
-        if (idx != i)  // Rod is neighbor of itself
-        {
-          if (cell.m_aux_rod.isTouchingRod(cell.getRod(idx)))
-          {
-            validPosition = false;
-            break;
-          }
-        }
-      }
-
-      if (validPosition)
-      {
-        // Update grid coordinates of rod
-        cell.m_grid.moveIndex(i, oldCoords, newCoords);
-        cell.m_bundle[i]= cell.m_aux_rod;
-        numSuccess++;
+        ++numSuccess;
       }
     }
   }
